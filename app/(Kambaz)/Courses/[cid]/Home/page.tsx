9bea10fed39e2994eb0701a6ad284bd4c8e4da1d
Cocochoiii <choi.coco@northeastern.edu>
2025-09-21 02:30:51 -0400

    fixed home page

--- app/(Kambaz)/Courses/[cid]/Home/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/page.tsx
@@ -1,12 +1,24 @@
-import Modules from "../Modules/page";
+import { ModulesContent } from "../Modules/page";
 import CourseStatus from "./Status";
-export default function Home() {
-  return (
-    <div id="wd-home">
-      <table><tbody><tr>
-        <td valign="top" width="70%"><Modules /></td>
-        <td valign="top"><CourseStatus /></td>
-      </tr></tbody></table>
-    </div>
-  );
+
+export default async function Home({
+                                       params,
+                                   }: { params: Promise<{ cid: string }> }) {
+    const { cid } = await params;
+    return (
+        <div id="wd-home">
+            <table>
+                <tbody>
+                <tr>
+                    <td valign="top" width="70%">
+                        <ModulesContent cid={cid} />
+                    </td>
+                    <td valign="top">
+                        <CourseStatus />
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
