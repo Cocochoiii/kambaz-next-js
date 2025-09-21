778287c8add5f57fa1299e260ec616e912afef4d
Cocochoiii <choi.coco@northeastern.edu>
2025-09-21 02:29:35 -0400

    fixed course layout.tsx

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,14 +1,17 @@
-import type { ReactNode } from "react";
-import CourseNavigation from "./Navigation";
-export default function CoursesLayout({ children }: Readonly<{ children: ReactNode }>) {
-  return (
-    <div id="wd-courses">
-      <h2>Courses 1234</h2>
-      <hr />
-      <table><tbody><tr>
-        <td valign="top" width="220"><CourseNavigation /></td>
-        <td valign="top" width="100%">{children}</td>
-      </tr></tbody></table>
-    </div>
-  );
+import CourseNavigation from "./CourseNavigation";
+
+export default async function CoursesLayout({
+                                                children, params
+                                            }: Readonly<{ children: React.ReactNode; params: Promise<{ cid: string }> }>) {
+    const { cid } = await params;
+    return (
+        <div id="wd-courses">
+            <h2>Courses {cid}</h2>
+            <hr/>
+            <table><tbody><tr>
+                <td valign="top" width="200"><CourseNavigation cid={cid}/></td>
+                <td valign="top" width="100%">{children}</td>
+            </tr></tbody></table>
+        </div>
+    );
 }
