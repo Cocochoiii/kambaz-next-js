c55ea0143df982dd976dba4ce471777b788c8597
Cocochoiii <choi.coco@northeastern.edu>
2025-09-21 03:09:36 -0400

    updated lab layout.tsx

--- app/Labs/layout.tsx
+++ app/Labs/layout.tsx
@@ -1,10 +1,15 @@
 import type { ReactNode } from "react";
 import TOC from "./TOC";
+
 export default function LabsLayout({ children }: Readonly<{ children: ReactNode }>) {
-  return (
-    <table><tbody><tr>
-      <td valign="top" width="180"><TOC /></td>
-      <td valign="top">{children}</td>
-    </tr></tbody></table>
-  );
+    return (
+        <table>
+            <tbody>
+            <tr>
+                <td valign="top" width="180"><TOC /></td>
+                <td valign="top">{children}</td>
+            </tr>
+            </tbody>
+        </table>
+    );
 }
