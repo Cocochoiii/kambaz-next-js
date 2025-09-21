8a43acf9f912e3b24794ecf87120ad773e5481e6
Cocochoiii <choi.coco@northeastern.edu>
2025-09-21 03:10:00 -0400

    updated dashboard page.tsx

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 const courses = [
-  { id: "5610", title: "CS5610 Web Development", tag: "Full Stack Web", img: "reactjs.jpg" },
-  { id: "5520", title: "CS5520 Mobile Application Development", tag: "Mobile Dev", img: "course1.jpg" },
-  { id: "5004", title: "CS5004 Object-Oriented Design", tag: "OOD in Java", img: "course2.jpg" },
-  { id: "5200", title: "CS5200 Database Management Systems", tag: "Relational DBMS", img: "course3.jpg" },
-  {id: "5800", title: "CS5800 Algorithms", tag: "Algorithm Analysis", img: "course4.jpg" },
-  { id: "6620", title: "CS6620 Fundamentals of Cloud Computing", tag: "Cloud Computing", img: "course5.jpg" },
-  { id: "6510", title: "CS6510 Advanced Software Dev", tag: "Large-scale projects", img: "course6.jpg" }
+  { id: "5610", title: "CS5610 Web Development", tag: "Full Stack Web", img: "course1.jpg" },
+  { id: "5520", title: "CS5520 Mobile Application Development", tag: "Mobile Dev", img: "course2.jpg" },
+  { id: "5004", title: "CS5004 Object-Oriented Design", tag: "OOD in Java", img: "course3.jpg" },
+  { id: "5200", title: "CS5200 Database Management Systems", tag: "Relational DBMS", img: "course4.jpg" },
+  {id: "5800", title: "CS5800 Algorithms", tag: "Algorithm Analysis", img: "course5.jpg" },
+  { id: "6620", title: "CS6620 Fundamentals of Cloud Computing", tag: "Cloud Computing", img: "course6.jpg" },
+  { id: "6510", title: "CS6510 Advanced Software Dev", tag: "Large-scale projects", img: "course7.jpg" }
 ];
 export default function Dashboard() {
   return (
