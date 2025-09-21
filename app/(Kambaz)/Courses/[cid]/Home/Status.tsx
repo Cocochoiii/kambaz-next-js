884d630c4284adec2fd16ee635a2ddc09a07c443
Cocochoiii <choi.coco@northeastern.edu>
2025-09-21 02:32:18 -0400

    fixed Status page

--- app/(Kambaz)/Courses/[cid]/Home/Status.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/Status.tsx
@@ -1,14 +1,17 @@
-export default function CourseStatus() {
-  return (
-    <div id="wd-course-status">
-      <h2>Course Status</h2>
-      <button>Unpublish</button> <button>Publish</button>
-      <div>
-        <button>Import Existing Content</button><br/>
-        <button>Import From Commons</button><br/>
-        <button>Choose Home Page</button><br/>
-      </div>
-      <button>View Course Notifications</button>
-    </div>
-  );
+export default function CourseStatus(){
+    return (
+        <div id="wd-course-status">
+            <h2>Course Status</h2>
+            <button>Unpublish</button> <button>Publish</button>
+            <div style={{ marginTop: 6, marginBottom: 6 }}>
+                <button>Import Existing Content</button><br/>
+                <button>Import from Commons</button><br/>
+                <button>Choose Home Page</button><br/>
+                <button>View Course Stream</button><br/>
+                <button>New Announcement</button><br/>
+                <button>New Analytics</button><br/>
+                <button>View Course Notifications</button>
+            </div>
+        </div>
+    );
 }
