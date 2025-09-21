39996edd990b93ba260a6ca44bcfa46c4cf33085
Cocochoiii <choi.coco@northeastern.edu>
2025-09-21 02:32:07 -0400

    fixed App page

--- app/(Kambaz)/page.tsx
+++ app/(Kambaz)/page.tsx
@@ -1,2 +1,5 @@
 import { redirect } from "next/navigation";
-export default function Kambaz() { redirect("/Account/Signin"); return <div id="wd-kambaz"><h1>Kambaz</h1></div>; }
+export default function Kambaz() {
+    redirect("/Account/Signin");
+    return <div id="wd-kambaz"><h1>Kambaz</h1></div>;
+}
