b973ca40bf195ef83d3330f324e4b2bceb0e5156
Cocochoiii <choi.coco@northeastern.edu>
2025-10-02 00:26:22 -0400

    update A5 Kambaz and labs5

--- app/Labs/Navigation.tsx
+++ app/Labs/Navigation.tsx
@@ -1,21 +1,32 @@
 // app/Labs/Navigation.tsx
-import { Nav, NavItem, NavLink } from "react-bootstrap";
+"use client";
+
+import Nav from "react-bootstrap/Nav";
 import Link from "next/link";
 
-export default function TOC() {
+export default function LabsNavigation() {
     return (
         <Nav variant="pills" className="flex-column">
-            <NavItem><NavLink href="/Labs" as={Link}>Labs</NavLink></NavItem>
-            <NavItem><NavLink href="/Labs/Lab1" as={Link}>Lab 1</NavLink></NavItem>
-            <NavItem><NavLink href="/Labs/Lab2" as={Link}>Lab 2</NavLink></NavItem>
-            <NavItem><NavLink href="/Labs/Lab3" as={Link}>Lab 3</NavLink></NavItem>
-            <NavItem><NavLink href="/Labs/Lab4" as={Link}>Lab 4</NavLink></NavItem>
-            <NavItem><NavLink href="/" as={Link}>Kambaz</NavLink></NavItem>
-            <NavItem>
-                <NavLink id="wd-github" href="https://github.com/Cocochoiii/kambaz-next-js">
-                    GitHub Repository
-                </NavLink>
-            </NavItem>
+            <Nav.Item><Nav.Link as={Link} href="/Labs">Labs</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link as={Link} href="/Labs/Lab1">Lab 1</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link as={Link} href="/Labs/Lab2">Lab 2</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link as={Link} href="/Labs/Lab3">Lab 3</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link as={Link} href="/Labs/Lab4">Lab 4</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link as={Link} href="/Labs/Lab5">Lab 5</Nav.Link></Nav.Item>
+
+            <Nav.Item><Nav.Link as={Link} href="/">Kambaz</Nav.Link></Nav.Item>
+
+            {/* External links */}
+            <Nav.Item>
+                <Nav.Link id="wd-github" href="https://github.com/Cocochoiii/kambaz-next-js" target="_blank">
+                    GitHub Repository (UI)
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="https://github.com/Cocochoiii/kambaz-node-server-app" target="_blank">
+                    GitHub Repository (Server)
+                </Nav.Link>
+            </Nav.Item>
         </Nav>
     );
 }
--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -1,17 +1,22 @@
 // app/Labs/page.tsx
+import Link from "next/link";
+
 export default function Labs() {
     return (
         <div id="wd-labs">
             <h1>Labs</h1>
             <p>Name: Coco Cai</p>
             <p>Section: Fall2025 Section 03</p>
+
             <ul>
-                <li><a id="wd-lab1-link" href="/Labs/Lab1">Lab 1: HTML</a></li>
-                <li><a id="wd-lab2-link" href="/Labs/Lab2">Lab 2: CSS & Bootstrap</a></li>
-                <li><a id="wd-lab3-link" href="/Labs/Lab3">Lab 3: JavaScript</a></li>
-                <li><a id="wd-lab4-link" href="/Labs/Lab4">Lab 4: State & Redux</a></li>
+                <li><Link id="wd-lab1-link" href="/Labs/Lab1">Lab 1: HTML</Link></li>
+                <li><Link id="wd-lab2-link" href="/Labs/Lab2">Lab 2: CSS &amp; Bootstrap</Link></li>
+                <li><Link id="wd-lab3-link" href="/Labs/Lab3">Lab 3: JavaScript</Link></li>
+                <li><Link id="wd-lab4-link" href="/Labs/Lab4">Lab 4: State &amp; Redux</Link></li>
+                <li><Link id="wd-lab5-link" href="/Labs/Lab5">Lab 5: HTTP Server &amp; REST APIs</Link></li>
             </ul>
-            <p><a id="wd-kambaz-home" href="/">Back to Kambaz</a></p>
+
+            <p><Link id="wd-kambaz-home" href="/">Back to Kambaz</Link></p>
         </div>
     );
 }
