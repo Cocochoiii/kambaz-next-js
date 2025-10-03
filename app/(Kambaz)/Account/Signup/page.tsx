1efeffa63ed0178d67278c771edb4ed766522df2
Cocochoiii <choi.coco@northeastern.edu>
2025-10-03 17:33:03 -0400

    set default value

--- app/(Kambaz)/Account/Signup/page.tsx
+++ app/(Kambaz)/Account/Signup/page.tsx
@@ -1,13 +1,43 @@
+// app/(Kambaz)/Account/Signup/page.tsx
+"use client";
+
 import Link from "next/link";
-export default function Signup(){
+
+export default function Signup() {
     return (
-        <div id="wd-signup-screen">
+        <div id="wd-signup-screen" className="pt-2">
             <h3>Sign up</h3>
-            <input placeholder="username" className="wd-username" /><br/>
-            <input placeholder="password" type="password" className="wd-password" /><br/>
-            <input placeholder="verify password" type="password" className="wd-password-verify" /><br/>
-            <Link href="/Account/Profile"> Sign up </Link><br/>
-            <Link href="/Account/Signin"> Sign in </Link>
+
+            <input
+                placeholder="username"
+                className="wd-username"
+                type="text"
+                defaultValue="new_user"
+                autoComplete="username"
+            />
+            <br />
+
+            <input
+                placeholder="password"
+                className="wd-password"
+                type="password"
+                defaultValue="123"
+                autoComplete="new-password"
+            />
+            <br />
+
+            <input
+                placeholder="verify password"
+                className="wd-password-verify"
+                type="password"
+                defaultValue="123"
+                autoComplete="new-password"
+            />
+            <br />
+
+            <Link href="/Account/Profile">Sign up</Link>
+            <br />
+            <Link href="/Account/Signin">Sign in</Link>
         </div>
     );
 }
