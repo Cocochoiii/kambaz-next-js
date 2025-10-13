8ef886eda443362cc9a4efffa6c9588b787c26e7
Cocochoiii <choi.coco@northeastern.edu>
2025-10-12 21:40:00 -0400

    fix vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 // next.config.mjs
+const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: { unoptimized: true },
     async rewrites() {
-        return [
-            // proxy all frontend /api/* calls to the Express server on :4000
-            { source: "/api/:path*", destination: "http://127.0.0.1:4000/api/:path*" },
-        ];
+        if (!isProd) {
+            return [{ source: "/api/:path*", destination: "http://127.0.0.1:4000/api/:path*" }];
+        }
+        return [];
     },
 };
-
 export default nextConfig;
