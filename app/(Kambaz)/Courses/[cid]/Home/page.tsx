eb23ad8c9e99a7a523c2da043277da1551cd87a3
Cocochoiii <choi.coco@northeastern.edu>
2026-07-09 22:15:31 -0400

    edit

--- app/(Kambaz)/Courses/[cid]/Home/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { useParams } from "next/navigation";
 import ListGroup from "react-bootstrap/ListGroup";
 import { BsGripVertical } from "react-icons/bs";
@@ -14,15 +14,10 @@ import * as db from "../../../Database";
 export default function HomePage() {
     const { cid } = useParams<{ cid: string }>();
     const course = db.courses.find((c: any) => c._id === cid);
+    // Show every module for this course (same as the Modules screen)
+    const modules = db.modules.filter((m: any) => m.course === cid);
 
-    // Get all modules for this course, but only show first 2 on Home
-    const allModules = db.modules.filter((m: any) => m.course === cid);
-    const modules = allModules.slice(0, 2);
-
-    // Collapse state management
-    const [collapsed, setCollapsed] = useState<boolean[]>(
-        () => modules.map(() => false)
-    );
+    const [collapsed, setCollapsed] = useState<boolean[]>(() => modules.map(() => false));
     const allCollapsed = collapsed.every(Boolean);
     const toggleAll = () => setCollapsed(collapsed.map(() => !allCollapsed));
     const toggleOne = (i: number) =>
@@ -38,70 +33,63 @@ export default function HomePage() {
             <p className="text-muted mt-1">{course.description}</p>
 
             <div className="d-flex gap-4">
-                {/* Main content column */}
+                {/* Main content column: full module list, rendered like the Modules screen */}
                 <div className="flex-fill">
-                    {/* Toolbar matching Modules */}
                     <div className="wd-toolbar btn-toolbar gap-2 my-2">
-                        <button
-                            className="btn btn-secondary"
-                            id="wd-home-collapse-all"
-                            onClick={toggleAll}
-                        >
+                        <button className="btn btn-secondary" id="wd-home-collapse-all" onClick={toggleAll}>
                             {allCollapsed ? "Expand All" : "Collapse All"}
                         </button>
-
                         <button className="btn btn-secondary" id="wd-home-view-progress">
                             View Progress
                         </button>
-
                         <PublishAllMenu idPrefix="wd-home" label="Publish All" />
-
                         <button className="btn btn-danger" id="wd-home-new-module">
                             + Module
                         </button>
                     </div>
 
-                    {/* Modules preview (first 2 modules) */}
-                    {modules.map((module: any, i: number) => (
-                        <ListGroup className="rounded-0 mb-4" key={module._id}>
-                            <ListGroup.Item className="p-0 border-gray">
-                                {/* Clickable header */}
+                    <ListGroup id="wd-home-modules" className="rounded-0">
+                        {modules.map((module: any, i: number) => (
+                            <ListGroup.Item key={module._id} className="wd-module p-0 mb-5 fs-5 border-gray">
+                                {/* header toggles the panel */}
                                 <button
                                     className="w-100 text-start border-0 p-0"
                                     onClick={() => toggleOne(i)}
                                     aria-expanded={!collapsed[i]}
                                     aria-controls={`wd-home-module-panel-${i}`}
                                 >
-                                    <div className="p-3 bg-secondary">
+                                    <div className="wd-title p-3 ps-2 bg-secondary">
                                         <BsGripVertical className="me-2 wd-grip" />
-                                        {module.name} <ModuleControlButtons />
+                                        {module.name}
+                                        <ModuleControlButtons />
                                     </div>
                                 </button>
 
-                                {/* Panel */}
-                                <div id={`wd-home-module-panel-${i}`} hidden={collapsed[i]}>
-                                    <ListGroup className="wd-lessons rounded-0">
-                                        <ListGroup.Item className="wd-lesson p-3 ps-1">
-                                            <div className="d-flex align-items-center">
+                                {module.lessons && (
+                                    <div id={`wd-home-module-panel-${i}`} hidden={collapsed[i]}>
+                                        <ListGroup className="wd-lessons rounded-0">
+                                            <ListGroup.Item className="wd-lesson p-3 ps-1">
                                                 <BsGripVertical className="me-2 wd-grip" />
                                                 <GreenCheckmark />
-                                                <span className="wd-title ms-2">LEARNING OBJECTIVES</span>
+                                                LEARNING OBJECTIVES
                                                 <LessonControlButtons />
-                                            </div>
-                                            <ul className="mt-2 mb-0">
-                                                {module.lessons && module.lessons.map((lesson: any) => (
-                                                    <li key={lesson._id}>{lesson.name}</li>
-                                                ))}
-                                            </ul>
-                                        </ListGroup.Item>
-                                    </ListGroup>
-                                </div>
+                                            </ListGroup.Item>
+                                            {module.lessons.map((lesson: any) => (
+                                                <ListGroup.Item key={lesson._id} className="wd-lesson p-3 ps-1">
+                                                    <BsGripVertical className="me-2 wd-grip" />
+                                                    {lesson.name}
+                                                    <LessonControlButtons />
+                                                </ListGroup.Item>
+                                            ))}
+                                        </ListGroup>
+                                    </div>
+                                )}
                             </ListGroup.Item>
-                        </ListGroup>
-                    ))}
+                        ))}
+                    </ListGroup>
                 </div>
 
-                {/* Right status column - hidden on smaller screens */}
+                {/* right status column, hidden below xl */}
                 <div id="wd-course-status-col" className="d-none d-xl-block" style={{ width: 340 }}>
                     <Status />
                 </div>
